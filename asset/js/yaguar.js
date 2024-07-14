807677759178ce6654408b3ba232ba3e08dfb63e
Harold A. Caraballo <electronicsistems15@gmail.com>
2024-07-13 21:41:02 -0400

    Primer commit 6

--- asset/js/yaguar.js
+++ asset/js/yaguar.js
@@ -48,3 +48,22 @@ $(document).ready(function () {
           ],
      });
 });
+
+
+//Botones
+
+$(function() {  
+     $('.btn-6')
+       .on('mouseenter', function(e) {
+         var parentOffset = $(this).offset(),
+             relX = e.pageX - parentOffset.left,
+             relY = e.pageY - parentOffset.top;
+         $(this).find('span').css({top:relY, left:relX})
+       })
+       .on('mouseout', function(e) {
+         var parentOffset = $(this).offset(),
+             relX = e.pageX - parentOffset.left,
+             relY = e.pageY - parentOffset.top;
+         $(this).find('span').css({top:relY, left:relX})
+       });
+   });
