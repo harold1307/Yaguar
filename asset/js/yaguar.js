ae265decc0d1966032322087a55081c639d11b27
Harold A. Caraballo <electronicsistems15@gmail.com>
2024-07-17 01:07:50 -0400

    Primer commit 10

--- asset/js/yaguar.js
+++ asset/js/yaguar.js
@@ -49,6 +49,57 @@ $(document).ready(function () {
      });
 });
 
+$(document).ready(function () {
+     $(".carousel_testimonios").slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          centerMode: true,
+          centerPadding: "20px",  
+          arrows: true,
+          responsive: [
+               {
+                    breakpoint: 1200,
+                    settings: {
+                         arrows: false,
+                         centerMode: true,
+                         centerPadding: "30px",
+                         slidesToShow: 3,
+                    },
+               },
+               {
+                    breakpoint: 992,
+                    settings: {
+                         arrows: false,
+                         centerMode: true,
+                         centerPadding: "20px",
+                         slidesToShow: 3,
+                    },
+               },
+               {
+                    breakpoint: 768,
+                    settings: {
+                         arrows: false,
+                         centerMode: true,
+                         centerPadding: "10px",
+                         slidesToShow: 1,
+                    },
+               },
+               {
+                    breakpoint: 480,
+                    settings: {
+                         arrows: false,
+                         centerMode: true,
+                         centerPadding: "10px",
+                         slidesToShow: 1,
+                    },
+               },
+          ],
+     });
+});
+
 //Botones
 
 $(function () {
